9069ace40a4f2de44fd2f3c57c38004a2f539d8c
micho1915 <mlugow.1915@gmail.com>
2026-04-12 12:54:15 +0200

    Mala optymalizacja metody pop_back w dynamic array oraz dodanie komentarzy z leksza na sile

--- DynamicArray.h
+++ DynamicArray.h
@@ -16,22 +16,22 @@ class DynamicArray{
     int* find(int value);
 
     private:
-        void resize();
+        void resize(); //metoda pomocnicza do powiekszenia tablicy
         int* data;
-        int size;
-        int capacity;
+        int size; //ilosc elementow
+        int capacity; //rozmiar tablicy
 };
 
 DynamicArray::DynamicArray(int s) : capacity(s), size(0){
-    data = new int[s];
+    data = new int[s]; //alokuje pamiec o rozmiarze s*sizeof(int) i przypisuje wskaznik tej pamieci do data
 }
 
 DynamicArray::~DynamicArray(){
-    delete[] data;
+    delete[] data; //trzeba zwolnic pamiec tablicy :)
 }
 
 void DynamicArray::push_back(int value){
-    if(size == capacity) resize();
+    if(size == capacity) resize(); //dodanie elementu, a co za tym idzie sprawdzenie czy nie wyjdzie poza rozmiar tablicy
 
     data[size] = value;
     size++;
@@ -40,24 +40,24 @@ void DynamicArray::push_back(int value){
 void DynamicArray::resize(){
     capacity = 2 * capacity;
 
-    int* tempData = new int[capacity];
+    int* tempData = new int[capacity]; //wskaznik na nowo zaalokowana pamiec dla tablicy o podwojonym rozmiarze
 
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < size; i++){ //kopiowanie wartosci ze starej tablicy do nowej
         tempData[i] = data[i];
     }
 
-    delete[] data;
+    delete[] data; //zwolnienie pamieci w ktorej miescila sie stara tablica
 
     data = tempData;
 }
 
 void DynamicArray::set(int index, int value){
-    if(index < 0 || index >= size) return;
+    if(index < 0 || index >= size) return; //sprawdza czy indeks miesci sie w rozmiarze tablicy
     data[index] = value;
 }
 
 int* DynamicArray::get(int index){
-    if(index < 0 || index >= size) return nullptr;
+    if(index < 0 || index >= size) return nullptr; //sprawdza czy indeks miesci sie w rozmiarze tablicy
     return &(data[index]);
 }
 
@@ -65,26 +65,26 @@ int* DynamicArray::find(int value){
     for(int i = 0; i < size; i++){
         if(data[i] == value) return &data[i];
     }
-    return nullptr;
+    return nullptr; //jak nie znajdzie elementu, zwraca nullptr
 }
 
 void DynamicArray::pop_at(int index){
-    if(index < 0 || index >= size) return;
-    for(int i = index; i < size-1; i++){
+    if(index < 0 || index >= size) return; 
+    for(int i = index; i < size-1; i++){//kazdy element od data[index] staje sie kolejnym elementem, efektywnie usuwa element data[index] 
         data[i] = data[i+1];
     }
     size--;
 }
 
 void DynamicArray::pop_back(){
-    pop_at(size-1);
+    size--; //
 }
 
 void DynamicArray::insert(int index, int value){
     if(index < 0 || index > size) return;
-    if(size == capacity) resize();
+    if(size == capacity) resize(); //dodanie elementu, wiec trzeba sprawdzic czy sie zmiesci i powiekszyc tablice jak nie
 
-    for(int i = size; i > index; i--){
+    for(int i = size; i > index; i--){//przesuwa wszystkie elementu od konca do data[index] wlacznie o 1 w prawo
         data[i] = data[i-1];
     }
 
